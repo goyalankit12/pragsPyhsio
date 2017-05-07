5a3cfc03d84b9ea4967edf5c5a9662e922ba90bb
goyalankit12 <goyalankit2012@gmail.com>
2017-05-07 01:44:03 -0700

    first commit

---
+++ index.php
@@ -0,0 +1,353 @@
+<!doctype html>
+<!--[if IE 7 ]>    <html lang="en-gb" class="isie ie7 oldie no-js"> <![endif]-->
+<!--[if IE 8 ]>    <html lang="en-gb" class="isie ie8 oldie no-js"> <![endif]-->
+<!--[if IE 9 ]>    <html lang="en-gb" class="isie ie9 no-js"> <![endif]-->
+<!--[if (gt IE 9)|!(IE)]><!-->
+<html lang="en-gb" class="no-js">
+<!--<![endif]-->
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+<!--[if lt IE 9]> 
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <![endif]-->
+<title>Prag's Physiotherapy</title>
+<meta name="description" content="">
+<meta name="author" content="WebThemez">
+<!--[if lt IE 9]>
+        <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+<!--[if lte IE 8]>
+		<script type="text/javascript" src="http://explorercanvas.googlecode.com/svn/trunk/excanvas.js"></script>
+	<![endif]-->
+<link rel="stylesheet" href="css/bootstrap.min.css" />
+<link rel="stylesheet" type="text/css" href="css/isotope.css" media="screen" />
+<link rel="stylesheet" href="js/fancybox/jquery.fancybox.css" type="text/css" media="screen" />
+<link href="css/animate.css" rel="stylesheet" media="screen">
+<!-- Owl Carousel Assets -->
+<link href="js/owl-carousel/owl.carousel.css" rel="stylesheet">
+<link rel="stylesheet" href="css/styles.css" />
+<!-- Font Awesome -->
+<link href="font/css/font-awesome.min.css" rel="stylesheet">
+<link rel="stylesheet" href="js/css/smoothness/jquery-ui-1.10.4.custom.css"> 
+  		<script src="js/js/jquery-1.10.2.js"></script>
+  		<script src="js/jquery.js"></script>
+  		<script src="js/js/jquery-ui-1.10.4.custom.min.js"></script> 
+		 <style>
+      #map {
+        width: 500px;
+        height: 400px;
+		 -webkit-border-radius: 10px;
+    -moz-border-radius: 10px;
+    border-radius: 10px;
+		
+      }
+    </style>
+    <script src="https://maps.googleapis.com/maps/api/js"></script>
+	<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-67304661-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+</head>
+
+<body onload="initialize();codeAddress()" >
+<header class="header">
+  <div class="container">
+    <nav class="navbar navbar-inverse" role="navigation">
+      <div class="navbar-header">
+        <button type="button" id="nav-toggle" class="navbar-toggle" data-toggle="collapse" data-target="#main-nav"> <span class="sr-only"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button>
+        <a href="#" class="navbar-brand scroll-top logo  animated bounceInLeft"><img width="20%" src="images/logo.png" alt="banner" /></a> </div>
+      <!--/.navbar-header-->
+      <div id="main-nav" class="collapse navbar-collapse">
+        <ul class="nav navbar-nav" id="mainNav">
+          <li class="active" id="firstLink"><a href="#home" class="scroll-link">Home</a></li>
+          <li><a href="#services" class="scroll-link">Services</a></li>
+          <li><a href="#aboutUs" class="scroll-link">About Us</a></li>
+          <li><a href="#work" class="scroll-link">Gallery</a></li>
+          <li><a href="#team" class="scroll-link">Doctor</a></li>
+          <li><a href="#contactUs" class="scroll-link">Contact Us</a></li>
+		 <li><a href="#" class="scroll-link">                 <div id="practo-widget-wrapper" data-widget-href="https://www.practo.com/other/doctor/anshu-singla-physiotherapist" ></div>
+                <script type="text/javascript">
+                    (function(d,s,id){
+                        var js, script = d.getElementsByTagName(s)[0];
+                        if(d.getElementById(id)) { return;}
+                        js = d.createElement(s);
+                        js.id = id;
+                        js.async = true;
+                        js.src = "https://www.practo.com/bundles/practopractoapp/js/marketing/doc-widget.js";
+			script.parentNode.insertBefore(js, script);
+                    })(document,'script','practo-widget-js');
+                </script>  </a></li>
+        </ul>
+	  </div>
+      <!--/.navbar-collapse--> 
+    </nav>
+    <!--/.navbar--> 
+  </div>
+  <!--/.container--> 
+</header>
+<!--/.header-->
+<div id="#top"></div>
+<section id="home">
+  <div class="banner-container"> <img src="images/banner-bg.jpg" alt="banner" />
+    <div class="container banner-content">
+      <div class="hero-text animated fadeInDownBig">
+        <h1  class="responsive-headline" style="font-size: 20px;">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;PRAG'S PHYSIOTHERAPY&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</h1>
+        <a href="#basics" class="arrow-link"> <i class="fa fa-arrow-circle-down fa-2x"></i> </a> 
+        <!--<p>your product and service.</p>--> 
+      </div>
+    </div>
+  </div>
+</section>
+<section id="services" class="page-section colord">
+  <div class="container">
+    <div class="heading text-center"> 
+      <!-- Heading -->
+      <h2>Our Services</h2>
+      <p>
+	  		Prag's Physiotherapy provides high quality physical therapy services with innovative wellness programming. Our orthopedic manual physical therapists treat a wide range of conditions, including low back and neck pain, pelvic and hip pain and sports injuries.
+	  </p>
+    </div>
+    <div class="row"> 
+      <!-- item -->
+      <div class="col-md-3 text-center c1"> <i class="fa  fa-2x circle"><img width="65%" src="images/2.png" alt="banner" /></i>
+        <h3><span class="id-color">Facilities We Have</span></h3>
+			  <table style="margin-left:20%;" align="center"   class="list" >
+				<tr><td><img class="bullet" src="images/greenBullet.png" alt="banner" /></td><td align="left" width="80%">LASER</td></tr>
+				<tr><td><img class="bullet" src="images/greenBullet.png" alt="banner" /></td><td align="left" >SWD</td></tr>
+				<tr><td><img class="bullet" src="images/greenBullet.png" alt="banner" /></td><td align="left" >ULTRA-SONIC</td></tr>
+				<tr><td><img class="bullet" src="images/greenBullet.png" alt="banner" /></td><td align="left">TENS</td></tr>
+				<tr><td><img class="bullet" src="images/greenBullet.png" alt="banner" /></td><td align="left">IIFT</td></tr>
+				<tr><td><img class="bullet" src="images/greenBullet.png" alt="banner" /></td><td align="left">MANUAL THERAPY</td></tr>
+				<tr><td><img class="bullet" src="images/greenBullet.png" alt="banner" /></td><td align="left">EXCERCISE</td></tr>
+				<tr><td><img class="bullet" src="images/greenBullet.png" alt="banner" /></td><td align="left">TRACTIONS</td></tr>
+			</table>
+      </div>
+      <!-- end: --> 
+      <!-- item -->
+      <div class="col-md-3 text-center c2"> <i class="fa  fa-2x circle"><img width="65%" src="images/1.png" alt="banner" /></i>
+        <h3><span class="id-color">Treatment We Provide</span></h3>
+			  <table align="center"   class="list" >
+				<tr><td><img class="bullet" src="images/greenBullet.png" alt="banner" /></td><td align="left" width="85%">CERVICAL NECK PAIN</td></tr>
+				<tr><td><img class="bullet" src="images/greenBullet.png" alt="banner" /></td><td align="left" >ANKLE SPRAIN</td></tr>
+				<tr><td><img class="bullet" src="images/greenBullet.png" alt="banner" /></td><td align="left" >BACK PAIN</td></tr>
+				<tr><td><img class="bullet" src="images/greenBullet.png" alt="banner" /></td><td align="left">FROZEN SHOULDER</td></tr>
+				<tr><td><img class="bullet" src="images/greenBullet.png" alt="banner" /></td><td align="left">TENNIS ELBOW</td></tr>
+				<tr><td><img class="bullet" src="images/greenBullet.png" alt="banner" /></td><td align="left">KNEE PAIN/ ARTHRITIS </td></tr>
+				<tr><td><img class="bullet" src="images/greenBullet.png" alt="banner" /></td><td align="left">STROKE/ PARALYSIS</td></tr>
+				<tr><td><img class="bullet" src="images/greenBullet.png" alt="banner" /></td><td align="left">SPORTS INJURIES</td></tr>
+			</table>
+      </div>
+      <!-- end: --> 
+      
+      <!-- item -->
+      <div class="col-md-3 text-center c1"> <i class="fa fa-2x circle"><img width="45%" src="images/5.png" alt="banner" /></i>
+        <h3><span class="id-color">Care At Your Home</span></h3>
+          <p  class="list"  style="text-align: justify;text-justify: inter-word;" >
+				Home Visit for physiotherapy is known for housing experienced Physiotherapist.<br/> Get an expert physiotherapist, using the latest techniques to treat you in the comfort of your home.
+      	 </p>	
+	  </div>
+      <!-- end: --> 
+      
+      <!-- item -->
+      <div class="col-md-3 text-center c2"> <i class="fa fa-2x circle"><img width="65%" src="images/4.png" alt="banner" /></i>
+        <h3><span class="id-color">Cryobanks Int.</span></h3>
+        <p class="list"  style="text-align: justify;text-justify: inter-word;" >
+		We are associated with Cryobanks International India.
+We are foccued on providing high-quality umbilical cord blood stem cell processing, and storage for both private & public donation.	
+			</p>
+      </div>
+      <!-- end:--> 
+    </div>
+  </div>
+  <!--/.container--> 
+</section>
+<section id="aboutUs">
+  <div class="container">
+    <div class="heading text-center"> 
+      <!-- Heading -->
+      <h2>About Us</h2>
+     
+    </div>
+    <div class="row feature design">
+      <div class="six columns right">
+       
+         <p style="text-align: justify;text-justify: inter-word;" >
+	  		Prag's physiotherapy is a private practice that combines traditional physical therapy services with innovative wellness programming.
+			We seeks to prevent and manage musculoskeletal pain and to enhance performance using evidence-based research, the highest-level technology, and cutting-edge orthopedic treatment techniques. 
+			Comprehensive assessments of stability, flexibility, and biomechanical function serve as the foundation of each client’s daily or sport-specific program. 
+			We develop lifelong relationships with our clients with the goal being optimum musculoskeletal health. 
+	  		<br/><br/>
+			For the past 8 years we have successfully transitioned our clients from painful conditions which they thought were “something I had to live with” 
+			into free-moving, pain-free fans. What makes us different from traditional physical therapy practices is our approach.  
+			We believe in the intimate connection between movement patterns and musculoskeletal pain to assess the source of the injury.
+		</p>
+       
+      </div>
+      <div class="six columns feature-media left"> <img src="images/physiotherapy.png" alt=""> </div>
+    </div>
+  </div>
+</section>
+<section id="work" class="page-section page">
+  <div class="container text-center">
+    <div class="heading">
+      <h2>Our Facilities</h2>
+      <p></p>
+    </div>
+    <div class="row">
+      <div class="col-md-12">
+        <div id="portfolio">
+          <ul class="items list-unstyled clearfix animated fadeInRight showing" data-animation="fadeInRight" style="position: relative; height: 438px;">
+            <li class="item branding" style="position: absolute; left: 0px; top: 0px;"> <a href="images/work/1.jpg" class="fancybox"> <img src="images/work/1.jpg" alt="">
+              <div class="overlay"> <span></span> </div>
+              </a> </li>
+            <li class="item photography" style="position: absolute; left: 292px; top: 0px;"> <a href="images/work/2.jpg" class="fancybox"> <img src="images/work/2.jpg" alt="">
+              <div class="overlay"> <span></span> </div>
+              </a> </li>
+            <li class="item branding" style="position: absolute; left: 585px; top: 0px;"> <a href="images/work/3.jpg" class="fancybox"> <img src="images/work/3.jpg" alt="">
+              <div class="overlay"> <span></span> </div>
+              </a> </li>
+            <li class="item photography" style="position: absolute; left: 877px; top: 0px;"> <a href="images/work/4.jpg" class="fancybox"> <img src="images/work/4.jpg" alt="">
+              <div class="overlay"> <span></span> </div>
+              </a> </li>
+            <li class="item photography" style="position: absolute; left: 0px; top: 219px;"> <a href="images/work/5.jpg" class="fancybox"> <img src="images/work/5.jpg" alt="">
+              <div class="overlay"> <span></span> </div>
+              </a> </li>
+            <li class="item web" style="position: absolute; left: 292px; top: 219px;"> <a href="images/work/6.jpg" class="fancybox"> <img src="images/work/6.jpg" alt="">
+              <div class="overlay"> <span></span> </div>
+              </a> </li>
+            <li class="item photography" style="position: absolute; left: 585px; top: 219px;"> <a href="images/work/7.jpg" class="fancybox"> <img src="images/work/7.jpg" alt="">
+              <div class="overlay"> <span></span> </div>
+              </a> </li>
+            <li class="item web" style="position: absolute; left: 877px; top: 219px;"> <a href="images/work/8.jpg" class="fancybox"> <img src="images/work/8.jpg" alt="">
+              <div class="overlay"> <span></span> </div>
+              </a> </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<section id="team" class="page-section">
+  <div class="container">
+    <div class="heading text-center"> 
+      <!-- Heading -->
+      <h2>Dr. Anshu Singla</h2>
+	  <span class="pos">Consultant Physiotherapist<br/>Ex NTPC, Vimhans</span>
+    </div>
+    <!-- Team Member's Details -->
+    <div class="team-content">
+      <div class="row" >
+           <!-- Designation --> 
+		<p style="text-align: justify;text-justify: inter-word;" > 
+			Dr. Anshu Singla served as Consulatnt physiotherapist at National Thermal Power Corporation(NTPC), New Delhi.
+			Upon completion of her tenure there, she joined Vimhans Hospital South Delhi and Alpha One hospital New Delhi as a Senior Physiotherapist. 
+			Currently, she is practicing at Prag's Physiotherapy Center. 
+			She is experienced working with infant through geriatric populations. 
+			She has credentials and experience in the treamanet of post-traumatic headaches, concussions, complex regional pain management.
+			She also provides gait analysis and prosthetic, orthotic and mobility assessment and prescription management. 
+		</p>
+
+  </div>
+  </div>
+  <!--/.container--> 
+</section>
+<section id="contactUs" class="contact-parlex">
+  <div class="parlex-back">
+    <div class="container">
+      <div class="row">
+        <div class="heading text-center"> 
+          <!-- Heading -->
+          <h2>Contact Us</h2>
+          <h3>Prag's Physiotherapy</h3>
+        </div>
+      </div>
+  <table>	
+  <tr>
+  	<td>
+  			 <script>
+				       function initialize() { // google maps
+						   map.style.border = '8px solid white';
+						    geocoder = new google.maps.Geocoder();
+						    var latlng = new google.maps.LatLng(-34.397, 150.644);
+						    var mapOptions = {
+						      zoom: 14,
+						      center: latlng
+					    }
+					    map = new google.maps.Map(document.getElementById("map"), mapOptions);
+					  }
+					  function codeAddress() { // address via geo location
+					     var address = "b block,sector 62, Noida";
+					   	  geocoder.geocode( { 'address': address}, function(results, status) {
+					      if (status == google.maps.GeocoderStatus.OK) {
+					        map.setCenter(results[0].geometry.location);
+					        var marker = new google.maps.Marker({
+				            map: map,
+				            position: results[0].geometry.location
+				        });
+				      } 
+				    });
+				  }
+			    </script>
+		 <div id="map"></div>
+  	</td>
+	<td width="30%"></td>
+	<td valign="top">
+          <h4>Address:</h4>
+          <address>
+         Prag's Physiotherapy<br><br>
+          Shop No. 4, 1st Floor,<br> 
+		  B Block Market,<br>
+		  Behind Fortis Hospital,<br>
+		  Sector-62,<br>
+          Noida<br>
+          </address>
+          <h4>Phone:</h4>
+          <address>
+          +91 9911053307<br>
+		  
+          </address>
+	  </td></tr>
+</table>   
+<br/>
+<br/>
+ <div class="container">
+      <div class="social text-center"> 
+	  	<a onclick="window.open('https://www.facebook.com/pragsphysioclinic')"><i class="fa fa-twitter"></i></a>
+		<a onclick="window.open('https://www.facebook.com/pragsphysioclinic')"><i class="fa fa-facebook"></i></a> 
+	</div>
+      <!--CLEAR FLOATS--> 
+    </div>
+    <!--/.container--> 
+  </div>
+</section>
+<!--/.page-section-->
+<section class="copyright">
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-12 text-center"> Copyright 2015 | All Rights Reserved Prag's Physiotherapy</div>
+    </div>
+    <!-- / .row --> 
+  </div>
+</section>
+<a href="#top" class="topHome"><i class="fa fa-chevron-up fa-2x"></i></a> 
+
+<!--[if lte IE 8]><script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script><![endif]--> 
+<script src="js/modernizr-latest.js"></script> 
+<script src="js/jquery-1.8.2.min.js" type="text/javascript"></script> 
+<script src="js/bootstrap.min.js" type="text/javascript"></script> 
+<script src="js/jquery.isotope.min.js" type="text/javascript"></script> 
+<script src="js/fancybox/jquery.fancybox.pack.js" type="text/javascript"></script> 
+<script src="js/jquery.nav.js" type="text/javascript"></script> 
+<script src="js/jquery.fittext.js"></script> 
+<script src="js/waypoints.js"></script> 
+<script src="js/custom.js" type="text/javascript"></script> 
+<script src="js/owl-carousel/owl.carousel.js"></script>
+</body>
+</html>
